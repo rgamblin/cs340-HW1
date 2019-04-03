d58702b3570422029c0b4f71720d84f6effcfeb6
Rodger Gamblin <gamblinr@oregonstate.edu>
2019-04-03 16:20:02 -0700

    Got a basic template into both programs

---
+++ insertsort.c
@@ -0,0 +1,21 @@
+/************************************************
+ * Author: Rodger L. Gamblin
+ * 
+ * File Name: insertionsort.c
+ * 
+ * Description: This program accepts input from a 
+ * file called "data.txt" and sorts it by insertion
+ * sort
+ *  
+ ************************************************/ 
+
+
+
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Hello world!\n");
+	return 0;
+}
---
+++ mergesort.c
@@ -0,0 +1,28 @@
+/************************************************
+ * Author: Rodger L. Gamblin
+ * File Name: mergesort.c
+ * Description: This is a program that takes an 
+ * array of numbers from a file called data.txt
+ * and sorts the numbers therein using mergesort 
+ *
+ *
+ *
+ * This is a base template for my C programs 
+ * 
+ ************************************************/ 
+
+
+
+#include <stdio.h>
+
+
+int main()
+{
+
+	
+
+	printf("Hello world!\n");
+	return 0;
+}
+
+
